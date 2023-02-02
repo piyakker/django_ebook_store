c1e45c2eb1cb89cf1e3f955dad6ebfcc2522519c
piyakker <ericlien86@gmail.com>
2023-02-02 23:10:25 +0800

    create Ebook model

--- eBook/Store/models.py
+++ eBook/Store/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Ebook(models.Model):
+    title = models.CharField(max_length=50)
+    price = models.FloatField()
+    cvr_url = models.CharField(max_length=2048)
