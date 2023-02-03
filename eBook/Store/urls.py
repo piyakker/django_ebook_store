2c2a75272ef7823b5ddcaabd261ec4ee7f2d0b99
piyakker <ericlien86@gmail.com>
2023-02-03 12:28:28 +0800

    add eBook to cart

--- eBook/Store/urls.py
+++ eBook/Store/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', store_view),
-    path('cart', cart_view)
+    path('cart', cart_view),
+    path('cart_add_item/', add_cart_item),
 ]
--- eBook/Store/views.py
+++ eBook/Store/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
-from .models import Ebook
+from django.shortcuts import render, HttpResponseRedirect
+from .models import Ebook, CartItem
+from django.views.decorators.csrf import csrf_exempt
 
 def store_view(request):
     ebooks = {'eBooks': Ebook.objects.all()}
     return render(request, 'Store/index.html', ebooks)
 
 def cart_view(request):
-    return render(request, 'Store/cart.html')
+    return render(request, 'Store/cart.html', {
+        'cart_items': CartItem.objects.all()
+    })
+
+@ csrf_exempt
+def add_cart_item(request):
+    book_id = request.POST['book_id']
+    title = Ebook.objects.get(pk=book_id).title
+    price = Ebook.objects.get(pk=book_id).price
+    item = CartItem.objects.create(title = title, price = price)
+    return HttpResponseRedirect('/store')
